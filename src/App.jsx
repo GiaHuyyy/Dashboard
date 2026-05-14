f4aeb91249ba8993492555a24529e675c58a0adb
Gia Huy <tovugiahuy@gmail.com>
2026-05-14 09:34:59 +0700

    Khởi tạo dự án

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-black">
+        <h1 className="text-5xl font-bold text-red-500">React + Tailwind</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
